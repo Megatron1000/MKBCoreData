e2fcbabf616a5a91bad39f6fb5cf9eccedc32976
Mark Bridges <mark.bridges@sainsburysentertainment.co.uk>
2015-08-17 12:07:01 +0100

    Initial commit

---
+++ Example/Pods/Target Support Files/MKBCoreData/MKBCoreData-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MKBCoreData : NSObject
+@end
+@implementation PodsDummy_MKBCoreData
+@end
---
+++ Example/Pods/Target Support Files/MKBCoreData/MKBCoreData-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double MKBCoreDataVersionNumber;
+FOUNDATION_EXPORT const unsigned char MKBCoreDataVersionString[];
+
